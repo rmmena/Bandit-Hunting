e2573b00bfc9f98f8ad1ef24564e98ae9d7d1770
rmmena <rodrigo.mena8632@outlook.com>
2018-11-08 21:21:34 -0200

    Adição das estruturas e suas inicializações

--- game/main.c
+++ game/main.c
@@ -10,6 +10,7 @@
 #define TAMSTRING 10
 #define NUM_ROCHAS 3
 #define MAX_BANDIDOS 3
+#define MAX_ESPECIAIS 5
 
 // ESTRUTURAS
 
@@ -19,7 +20,40 @@ typedef struct coordenada
     int coordenaday;
 } COORDENADA;
 
+typedef struct jogador
+{
+    char* nome[TAMSTRING];
+    COORDENADA posicao_xerife;
+    int velocidade;
+} JOGADOR;
+
+typedef struct bandido
+{
+    COORDENADA posicao_bandido;
+    int na_cadeia;
+    int velocidade;
+} BANDIDO;
+
+typedef struct especial
+{
+    COORDENADA posicao_especial;
+    int status;
+    int velocidade;
+} ESPECIAL;
+
 int main()
 {
+    // Inicializacao das estruturas
+
+    COORDENADA desloc;
+    JOGADOR xerife;
+    COORDENADA rochas[NUM_ROCHAS] = {{10, 10}, {20, 20}, {15, 15}};
+    BANDIDO bandidos[MAX_BANDIDOS] = {{5, 5}, {16,21}, {12, 17}};
+    ESPECIAL especiais[MAX_ESPECIAIS] = {{3,3}, {11,12}, {3,6}, {6,7}, {8,9}};
+
+    // Posicao inicial do xerife no jogo
+    xerife.posicao_xerife.coordenadax = 30;
+    xerife.posicao_xerife.coordenaday = 13;
+
 
 }
