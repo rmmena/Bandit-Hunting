1a0d85f178be339868f90f0dbc145e448fa31598
rmmena <rodrigo.mena8632@outlook.com>
2018-11-08 21:02:23 -0200

    #4 - Estrutura Coordenada

--- game/main.c
+++ game/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <conio2.h>
+#include <windows.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
 
+#define LINHA 24
+#define COLUNA 80
+#define TAMSTRING 10
+#define NUM_ROCHAS 3
+#define MAX_BANDIDOS 3
+
+// ESTRUTURAS
+
+typedef struct coordenada
+{
+    int coordenadax;
+    int coordenaday;
+} COORDENADA;
+
+int main()
+{
+
+}
